4c410829fb7989e909d13a1ee10564bb72cfe183
subharthi75 <subharthi75@gmail.com>
2024-03-30 22:14:12 +0530

    file up fixxxx

--- backend/routes/startup.js
+++ backend/routes/startup.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const createStartup = require("../controllers/createStartup");
 const isActiveStartup = require("../controllers/isActiveStartup");
+const { uploadFiles } = require("../utils/uploadFile");
 const router = express.Router();
 
-router.post("/create", createStartup);
+router.post("/create", uploadFiles, createStartup);
 router.get("/isactive/:useruid", isActiveStartup);
 
 module.exports = router;
