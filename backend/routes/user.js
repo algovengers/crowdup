168a5ba8748768c24d60f00decf91b84a4c10a22
Subharthi Hazra <92659226+subharthihazra@users.noreply.github.com>
2024-03-31 11:35:07 +0530

    rechange + payment (#7)
    * file up frontend fixxx
    
    * ...
    
    * schema
    
    * payment + recharge
    
    * route add

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const createUser = require("../controllers/createUser");
 const getUser = require("../controllers/getUser");
+const rechargeWallet = require("../controllers/rechargeWallet");
+const payment = require("../controllers/payment");
 const router = express.Router();
 
 router.post("/adduser", createUser);
 router.get("/getuser/:useruid", getUser);
+router.post("/recharge", rechargeWallet);
+router.post("/payment", payment);
 
 module.exports = router;
